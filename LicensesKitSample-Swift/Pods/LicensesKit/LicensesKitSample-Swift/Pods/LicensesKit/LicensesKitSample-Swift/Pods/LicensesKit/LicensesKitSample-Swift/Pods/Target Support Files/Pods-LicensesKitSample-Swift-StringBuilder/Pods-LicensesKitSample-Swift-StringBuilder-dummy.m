f60a0d0174a9c097e672c14574a974e02244e902
Matthew Wyskiel <mwyskiel@optonline.net>
2015-02-04 19:51:06 -0500

    pod spec update

---
+++ LicensesKitSample-Swift/Pods/LicensesKit/LicensesKitSample-Swift/Pods/LicensesKit/LicensesKitSample-Swift/Pods/LicensesKit/LicensesKitSample-Swift/Pods/Target Support Files/Pods-LicensesKitSample-Swift-StringBuilder/Pods-LicensesKitSample-Swift-StringBuilder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LicensesKitSample_Swift_StringBuilder : NSObject
+@end
+@implementation PodsDummy_Pods_LicensesKitSample_Swift_StringBuilder
+@end
