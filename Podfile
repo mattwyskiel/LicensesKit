d916fc0c9cde8611fab43c65085560794fb6f06c
Matthew Wyskiel <mwyskiel@optonline.net>
2015-02-09 16:28:14 -0500

    updated main project to use CocoaPods, as opposed to Submodules

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '8.0'
+
+target 'LicensesKit' do
+pod 'StringBuilder'
+end
+
+target 'LicensesKitTests' do
+pod 'StringBuilder'
+end
+
---
+++ Pods/Target Support Files/Pods-LicensesKitTests-StringBuilder/Pods-LicensesKitTests-StringBuilder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LicensesKitTests_StringBuilder : NSObject
+@end
+@implementation PodsDummy_Pods_LicensesKitTests_StringBuilder
+@end
